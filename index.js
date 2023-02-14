8189d6507c3a6396f51af86fa7e5c6b5b9d063a6
Brian Reardon <Brian.D.Reardon@gmail.com>
2023-02-14 17:56:45 -0500

    commit tree 8813a0692487c7ed9c3545c777bf6f4bf112cf55

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.id = "test";
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Reardon is the champion";
